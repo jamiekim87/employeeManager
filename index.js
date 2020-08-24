c1f4cb96cd4cadc16ffc6e84f5dcea2e715a0ed3
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-08-24 13:37:48 -0700

    commit

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-onst { prompt } = require('inquirer')
+const { prompt } = require('inquirer')
+const mysql = require('mysql2')
+require('console.table')
+
+const db = mysql.createConnection('mysql://root:rootroot@localhost/employee_db')
 
 const mainMenu = () => {
   prompt([
@@ -67,11 +71,76 @@ const mainMenu = () => {
 }
 
 const viewEmployees = () => {
-
+  db.query(`
+    SELECT employee.id, employee.first_name, employee.last_name,
+      role.title, role.salary, department.name AS department,
+      CONCAT(manager.first_name, ' ', manager.last_name) AS manager
+    FROM employee
+    LEFT JOIN role
+    ON employee.role_id = role.id
+    LEFT JOIN department
+    ON role.department_id = department.id
+    LEFT JOIN employee manager
+    ON manager.id = employee.manager_id
+  `, (err, employees) => {
+    if (err) { console.log(err) }
+    console.table(employees)
+    mainMenu()
+  })
 }
 
 const addEmployee = () => {
+  db.query('SELECT * FROM role', (err, roles) => {
+    if (err) { console.log(err) }
 
+    roles = roles.map(role => ({
+      name: role.title,
+      value: role.id
+    }))
+
+    db.query('SELECT * FROM employee', (err, employees) => {
+
+      employees = employees.map(employee => ({
+        name: `${employee.first_name} ${employee.last_name}`,
+        value: employee.id
+      }))
+      
+      employees.unshift({ name: 'None', value: null })
+
+      prompt([
+        {
+          type: 'input',
+          name: 'first_name',
+          message: 'What is the employee first name?'
+        },
+        {
+          type: 'input',
+          name: 'last_name',
+          message: 'What is the employee last name?'
+        },
+        {
+          type: 'list',
+          name: 'role_id',
+          message: 'Choose a role for the employee:',
+          choices: roles
+        },
+        {
+          type: 'list',
+          name: 'manager_id',
+          message: 'Choose a manager for the employee:',
+          choices: employees
+        }
+      ])
+        .then(employee => {
+          db.query('INSERT INTO employee SET ?', employee, (err) => {
+            if (err) { console.log(err) }
+            console.log('Employee Created!')
+            mainMenu()
+          })
+        })
+        .catch(err => console.log(err))
+    })
+  })
 }
 
 const updateEmployeeRole = () => {
@@ -94,4 +163,3 @@ const addRole = () => {
 
 }
 
-mainMenu()
