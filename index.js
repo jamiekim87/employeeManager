9afab6fa78c2fb89ef9ee5d28939d8cb06783eec
Jamie Kim <jamiekim@Jamies-MacBook-Air.local>
2020-08-24 13:00:17 -0700

    push

---
+++ index.js
@@ -0,0 +1,97 @@
+onst { prompt } = require('inquirer')
+
+const mainMenu = () => {
+  prompt([
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'Choose an action:',
+      choices: [
+        {
+          name: 'View Employees',
+          value: 'viewEmployees'
+        },
+        {
+          name: 'Add An Employee',
+          value: 'addEmployee'
+        },
+        {
+          name: `Update An Employee's Role`,
+          value: 'updateEmployeeRole'
+        },
+        {
+          name: 'View Departments',
+          value: 'viewDepartments'
+        },
+        {
+          name: 'Add A Department',
+          value: 'addDepartment'
+        },
+        {
+          name: 'View Roles',
+          value: 'viewRoles'
+        },
+        {
+          name: 'Add A Role',
+          value: 'addRole'
+        }
+      ]
+    }
+  ])
+    .then(({ choice }) => {
+      switch (choice) {
+        case 'viewEmployees':
+          viewEmployees()
+          break
+        case 'addEmployee':
+          addEmployee()
+          break
+        case 'updateEmployeeRole':
+          updateEmployeeRole()
+          break
+        case 'viewDepartments':
+          viewDepartments()
+          break
+        case 'addDepartment':
+          addDepartment()
+          break
+        case 'viewRoles':
+          viewRoles()
+          break
+        case 'addRole':
+          addRole()
+          break
+      }
+    })
+    .catch(err => console.log(err))
+}
+
+const viewEmployees = () => {
+
+}
+
+const addEmployee = () => {
+
+}
+
+const updateEmployeeRole = () => {
+
+}
+
+const viewDepartments = () => {
+
+}
+
+const addDepartment = () => {
+
+}
+
+const viewRoles = () => {
+
+}
+
+const addRole = () => {
+
+}
+
+mainMenu()
